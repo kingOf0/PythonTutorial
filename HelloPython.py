6ef45c78c47706c6b46d74e148a58e3c53668c04
kingOf0 <ibrahimbugra1077@gmail.com>
2022-01-03 22:05:13 +0300

    Değişken tanımlamak

--- HelloPython.py
+++ HelloPython.py
@@ -1,9 +1,13 @@
 # Bu bir yorum satırıdır. Yorum satırları '#' ile başlar. Python, bu satırları görmezden gelir.
 
-
 # Değişkenler Bir veriyi hafızada depolamak için değişkenleri kullanırız. Değişken isimlendirmede bir kaç kural
 # vardır. Örneğin sayı veya özel işaretler ile başlayamaz veya pythodaki bazı özel anlamlara gelen kelimelerden
 # oluşamaz.
+
+# Değişken Tanımlamak.
+# 'değişken_ismi = değer' şeklinde kullanılır. Buna 'assign' veya 'assignment' yani 'atama' denir.
+# Aşağıda x adında bir değişken oluşturup bunun değerini 5 olarak atadık.
+x = 5
 
 
 
