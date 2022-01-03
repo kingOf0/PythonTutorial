123edf4ff1fe14f227a5f5b6955c5e567ec671a8
kingOf0 <ibrahimbugra1077@gmail.com>
2022-01-03 22:06:02 +0300

    Sayılarla Toplama işlemi

--- HelloPython.py
+++ HelloPython.py
@@ -9,10 +9,9 @@
 # Aşağıda x adında bir değişken oluşturup bunun değerini 5 olarak atadık.
 x = 5
 
-
-
-
-
-
+# Matematik
+# Python'da bir çok cebirsel ifade beklendiği gibi çalışır. Örneğin '+' işareti iki veriyi toplayacaktır.
+# Bu toplama sayılarda üstüne ekleme, metinlerde ardına ekleme şeklindedir.
+y = x + 2  # y'nin değeri 4 olacaktır. Çünkü x'in değeri 2 olduğunda 2 + 2 = 4 dür.
 
 
