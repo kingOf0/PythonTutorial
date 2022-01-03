f88ab24f45aa3c63d458351a92d9b539cd3e1d35
kingOf0 <ibrahimbugra1077@gmail.com>
2022-01-03 22:04:44 +0300

    Değişkenler

--- HelloPython.py
+++ HelloPython.py
@@ -1,6 +1,11 @@
 # Bu bir yorum satırıdır. Yorum satırları '#' ile başlar. Python, bu satırları görmezden gelir.
 
 
+# Değişkenler Bir veriyi hafızada depolamak için değişkenleri kullanırız. Değişken isimlendirmede bir kaç kural
+# vardır. Örneğin sayı veya özel işaretler ile başlayamaz veya pythodaki bazı özel anlamlara gelen kelimelerden
+# oluşamaz.
+
+
 
 
 
