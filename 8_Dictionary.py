e6c281be8a5063277d3f5b90268e7a827c159770
kingOf0 <ibrahimbugra1077@gmail.com>
2022-02-02 03:56:54 +0300

    immutable keys of dictionary

--- 8_Dictionary.py
+++ 8_Dictionary.py
@@ -1,10 +1,10 @@
 # Dictionary | Sözlük
 #
-# Birden fazla veriyi ikili (Anahtar - Değer) sistemde tutar.
+# Birden fazla veriyi ikili (Anahtar - Değer) sistemde saklar.
 #
 # Boş bir dictionary, aşağıdaki syntax ile oluşturulur.
 dict = {}
 #
-#
+# Sözlük anahtarları Immutable dediğimiz "değişmez" tipte veriler olmak zorundadır. True, False, String, Int gibi.
 #
 #
